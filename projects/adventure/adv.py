cfa58f43edc9c5c6b0001cc89ff03182713879d2
Tate Keller <tatek1993@gmail.com>
2020-08-14 11:42:09 -0500

    tests passing in 100 moves, MVP

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -30,80 +30,87 @@ player = Player(world.starting_room)
 traversal_path = []
 visited = {}
 last_unexplored_room = []
-starting = True
+
 breadcrumbs = []
 
 
 # adds a room to visited
 def add_to_visited(current_room):
+    # add the current room id to visited if it is not there already
     if current_room.id not in visited:
         visited[current_room.id] = {}
+        # for each exit in the current room, use the exit direction as the key, and set the value of each to '?'
         for e in current_room.get_exits():
             visited[current_room.id][e] = '?'
-    # print('added to visited')
 
 
+# use this fn to return to the last place that has unexplored exits
 def travel_reverse():
+    # while the current room id is not the most recent element appended to l_u_r
     while player.current_room.id != last_unexplored_room[-1]:
+        # remove and return the last element of breadcrumbs
         crumb = breadcrumbs.pop()
+        # use that crumb (which is a direction) to move the player
         player.travel(crumb)
-        # print('room id', player.current_room.id)
+        # add that direction to our traversal_path
         traversal_path.append(crumb)
-    # we remove the room from l_u_r each tie we back track
+
+    # we remove the room from l_u_r each time we back track
     last_unexplored_room.pop()
 
 
 def get_unexplored_exits():
     # find the exits for the current room
     exits = player.current_room.get_exits()
-    res = [e for e in exits if visited[player.current_room.id][e] == '?']
-    # print('GUE', res)
-    # print('lur', last_unexplored_room)
-    return res
+    # remove all exits that have already been visited
+    # filter the exit list to only include exits with a value of '?'
+    return [e for e in exits if visited[player.current_room.id][e] == '?']
 
 
 add_to_visited(player.current_room)
 
+# while the length of visited < the # of rooms in room_graph i.e. we have not visited all rooms
 while len(visited) < len(room_graph):
 
+    # while there are still unexplored exits in the current room
     while len(get_unexplored_exits()) > 0:
-        # starting = False
+
         exits = get_unexplored_exits()
         # figure out the next room the player is moving to
-        # exist is a list of directions we can move. The first element is the direction we move in
+        # exits is a list of directions we can move. The first element is the direction we move in
         direction = exits[0]
-        # if theres more than one way we can move
+
+        # if theres more than one way we can move, we must remember this room because we must return here to explore other exits
         if len(exits) > 1:
-            # we slice off the first element, because we will visit that one
-            exits = exits[1:]
+            # only add a room to l_u_r if it is not already there
             if player.current_room.id not in last_unexplored_room:
                 # we're appending all rooms with unexplored exits
                 last_unexplored_room.append(player.current_room.id)
-                # print("lur", last_unexplored_room)
 
-        # move to the room in that direction
+        # remember the last room we were in to establish connection later
         prevroom = player.current_room
         reverse = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
 
         # travel
         player.travel(direction)
-        # print('ROOM ID', player.current_room.id)
+
         traversal_path.append(direction)
-        # print('move')
 
         # add to visited
         add_to_visited(player.current_room)
-        # print('add to visited 2')
 
         # make the room connections
+        # in the previous room, moving in the direction we were given takes us to our current room
         visited[prevroom.id][direction] = player.current_room.id
+        # in our current room, moving in the opposite direction will take us to the previous room
         visited[player.current_room.id][reverse[direction]] = prevroom.id
+
+        # leave a trail of breadcrumbs to backtrack later. These are the directions on how to get back to where we started
         breadcrumbs.append(reverse[direction])
 
     if len(last_unexplored_room) == 0:
-        # we will return traversal_path here, signifying that we are done! :O
+        # we are done! :O
         pass
-        print(len(traversal_path))
     else:
         travel_reverse()
 
